6d201b52ee7bbf1cf73252b6c27cb79d26f06ecf
Anderson Lima <arolima546@gmail.com>
2025-07-08 20:36:57 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Footer } from './components/footer/footer';
+import { Filme } from './components/filme/filme';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Filme],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+//Decorato, o component informa como o seu componente esta estruturado.
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+}
