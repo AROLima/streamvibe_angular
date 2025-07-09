abcb91f64a587eaec80af2dc4b5c0ae3ea71f73c
Anderson Lima <arolima546@gmail.com>
2025-07-08 21:33:59 -0300

    create header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Footer } from './components/footer/footer';
 import { Filme } from './components/filme/filme';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Filme],
+  imports: [Header, Footer, Filme],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
